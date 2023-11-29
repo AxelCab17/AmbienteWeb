c6fca9e09e683b4b51dc8dbc8b8645cb5ba7a325
AdriiViquez <101229435+AdriiViquez@users.noreply.github.com>
2023-11-29 13:14:38 -0600

    Se borro categorias

--- tienda_bowser/funciones.php
+++ tienda_bowser/funciones.php
@@ -1,7 +1,6 @@
 <?php 
 function getMenu(){
     $menu = array(array("url" => "index.php", "name" => "Inicio"), array("url" => "nosotros.php", "name" => "Nosotros"));
-    $menu[] = array("url" => "categorias.php", "name" => "Categorias");
     $menu[] = array("url" => "juegoscrud.php", "name" => "Juegos");
     $menu[] = array("url" => "accesoriosCrud.php", "name" => "Accesorios");
     $menu[] = array("url" => "consolasCrud.php", "name" => "Consolas");

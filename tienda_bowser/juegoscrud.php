d192135b36386cc777027e498916da35faee5d08
AdriiViquez <101229435+AdriiViquez@users.noreply.github.com>
2023-11-30 19:22:56 -0600

    Hecho pestaña nostros y arregle el menu de los crud de accesorios, juegos y consolas

--- tienda_bowser/juegoscrud.php
+++ tienda_bowser/juegoscrud.php
@@ -10,15 +10,22 @@ $menu = getMenu();
 <html lang="es">
 
 <head th:fragment="head">
-    <title>Tienda Bowser - Juegos</title>
+    <title>Tienda Bowser</title>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
+    <script src="js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 
 <body>
 <header th:fragment="header" class="header">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-danger bg-dark">
             <a class="logo navbar-brand" href="#">
                 <img src="https://cdn.discordapp.com/attachments/1165381757655318588/1165405392671612969/3bW7WPi.png?ex=6546bb59&is=65344659&hm=ac23a5d55495c86e72cfb26cde8d26778547d6d94563612029d304492f5109c9&"
                     class="logo" alt="Tienda Bowser">
@@ -29,7 +36,7 @@ $menu = getMenu();
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse ml-auto" id="navbarNav">
+            <div class="menu collapse navbar-collapse ml-auto" id="navbarNav">
                 <ul class="navbar-nav">
                     <?php foreach ($menu as $item) { ?>
                         <li class="nav-item"><a class="nav-link" href="<?php echo $item["url"] ?>">
@@ -42,7 +49,7 @@ $menu = getMenu();
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Consola
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="consolaDropdown">
+                        <div class="dropdown-menu bg-dark" aria-labelledby="consolaDropdown">
                             <a class="dropdown-item" href="#">Agregar Consola</a>
                             <a class="dropdown-item" href="#">Ver Consola</a>
                             <a class="dropdown-item" href="#">Editar Consola</a>
@@ -54,7 +61,7 @@ $menu = getMenu();
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Juego
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="juegoDropdown">
+                        <div class="dropdown-menu bg-dark" aria-labelledby="juegoDropdown">
                             <a class="dropdown-item" href="#">Agregar Juego</a>
                             <a class="dropdown-item" href="#">Ver Juego</a>
                             <a class="dropdown-item" href="#">Editar Juego</a>
@@ -66,7 +73,7 @@ $menu = getMenu();
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Accesorio
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="accesorioDropdown">
+                        <div class="dropdown-menu bg-dark" aria-labelledby="accesorioDropdown">
                             <a class="dropdown-item" href="accesoriosCrud.php">Agregar Accesorio</a>
                             <a class="dropdown-item" href="#">Ver Accesorio</a>
                             <a class="dropdown-item" href="#">Editar Accesorio</a>
@@ -78,65 +85,66 @@ $menu = getMenu();
                 <span id="themeIcon" class="fas fa-moon"></span>
             </button>
         </nav>
-        <div class="container mt-5">
-            <h2 class="mb-4">Juegos</h2>
+    </header>
+    <div class="container mt-5">
+        <h2 class="mb-4">Juegos</h2>
 
-            <!-- Agregar Juego Formulario -->
-            <form action="" method="post" class="bg-light p-4 rounded">
-    <h3>Agregar Juego</h3>
-    <div class="form-group">
-        <label for="nombre">Nombre del Juego:</label>
-        <input type="text" id="nombre" name="nombre" class="form-control" required>
-    </div>
+        <!-- Agregar Juego Formulario -->
+        <form action="" method="post" class="bg-light p-4 rounded">
+            <h3>Agregar Juego</h3>
+            <div class="form-group">
+                <label for="nombre">Nombre del Juego:</label>
+                <input type="text" id="nombre" name="nombre" class="form-control" required>
+            </div>
 
-    <div class="form-group">
-        <label for="consola">Consola:</label>
-        <input type="text" id="consola" name="consola" class="form-control" required>
-    </div>
+            <div class="form-group">
+                <label for="consola">Consola:</label>
+                <input type="text" id="consola" name="consola" class="form-control" required>
+            </div>
 
-    <div class="form-group">
-        <label for="precio">Precio:</label>
-        <input type="number" step="0.01" id="precio" name="precio" class="form-control" required>
-    </div>
+            <div class="form-group">
+                <label for="precio">Precio:</label>
+                <input type="number" step="0.01" id="precio" name="precio" class="form-control" required>
+            </div>
 
-    <div class="form-group">
-        <label for="existencias">Existencias:</label>
-        <input type="number" id="existencias" name="existencias" class="form-control" required>
-    </div>
+            <div class="form-group">
+                <label for="existencias">Existencias:</label>
+                <input type="number" id="existencias" name="existencias" class="form-control" required>
+            </div>
 
-    <div class="form-group">
-        <label for="ruta_imagen">URL Imagen:</label>
-        <input type="text" id="ruta_imagen" name="ruta_imagen" class="form-control">
-    </div>
+            <div class="form-group">
+                <label for="ruta_imagen">URL Imagen:</label>
+                <input type="text" id="ruta_imagen" name="ruta_imagen" class="form-control">
+            </div>
 
-    <button type="submit" name="agregar" class="btn btn-danger">Agregar Juego</button>
-</form>
+            <button type="submit" name="agregar" class="btn btn-danger">Agregar Juego</button>
+        </form>
 
-            <!-- Lista de Juegos -->
-            <h3 class="mt-4">Lista de Juegos</h3>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Consola</th>
-                        <th>Precio</th>
-                        <th>Existencias</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $juegos = obtenerJuegos($conexion);
+        <!-- Lista de Juegos -->
+        <h3 class="mt-4">Lista de Juegos</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Consola</th>
+                    <th>Precio</th>
+                    <th>Existencias</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $juegos = obtenerJuegos($conexion);
 
-                    while ($row = mysqli_fetch_assoc($juegos)) {
-                        echo "<tr>";
-                        echo "<td>" . $row['id_juego'] . "</td>";
-                        echo "<td>" . $row['nombre'] . "</td>";
-                        echo "<td>" . $row['consola'] . "</td>";
-                        echo "<td>$" . $row['precio'] . "</td>";
-                        echo "<td>" . $row['existencias'] . "</td>";
-                        echo "<td>
+                while ($row = mysqli_fetch_assoc($juegos)) {
+                    echo "<tr>";
+                    echo "<td>" . $row['id_juego'] . "</td>";
+                    echo "<td>" . $row['nombre'] . "</td>";
+                    echo "<td>" . $row['consola'] . "</td>";
+                    echo "<td>$" . $row['precio'] . "</td>";
+                    echo "<td>" . $row['existencias'] . "</td>";
+                    echo "<td>
                                 <form action='' method='post'>
                                     <input type='hidden' name='id_juego' value='" . $row['id_juego'] . "'>
                                     <button type='submit' name='ver' class='btn btn-info btn-sm'>Ver</button>
@@ -144,20 +152,20 @@ $menu = getMenu();
                                     <button type='submit' name='eliminar' class='btn btn-danger btn-sm'>Eliminar</button>
                                 </form>
                               </td>";
-                        echo "</tr>";
-                    }
-                    ?>
-                </tbody>
-            </table>
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
 
-            <!-- Ver, Editar, Eliminar Juego Formulario -->
-            <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST" && (isset($_POST["ver"]) || isset($_POST["editar"]))) {
-                $id_juego = $_POST["id_juego"];
-                $juego = obtenerJuegoPorId($conexion, $id_juego);
-                $row = mysqli_fetch_assoc($juego);
+        <!-- Ver, Editar, Eliminar Juego Formulario -->
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && (isset($_POST["ver"]) || isset($_POST["editar"]))) {
+            $id_juego = $_POST["id_juego"];
+            $juego = obtenerJuegoPorId($conexion, $id_juego);
+            $row = mysqli_fetch_assoc($juego);
 
-                echo "<form action='' method='post' class='bg-light p-4 rounded'>
+            echo "<form action='' method='post' class='bg-light p-4 rounded'>
                         <h3>Detalles del Juego</h3>
                         <div class='form-group'>
                             <label for='nombre'>Nombre del Juego:</label>
@@ -186,12 +194,12 @@ $menu = getMenu();
 
                         <button type='submit' name='editar' class='btn btn-warning'>Guardar Cambios</button>
                       </form>";
-            } elseif ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["ver"])) {
-                $id_juego = $_POST["id_juego"];
-                $juego = obtenerJuegoPorId($conexion, $id_juego);
-                $row = mysqli_fetch_assoc($juego);
+        } elseif ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["ver"])) {
+            $id_juego = $_POST["id_juego"];
+            $juego = obtenerJuegoPorId($conexion, $id_juego);
+            $row = mysqli_fetch_assoc($juego);
 
-                echo "<div class='bg-light p-4 rounded'>
+            echo "<div class='bg-light p-4 rounded'>
                         <h3>Detalles del Juego</h3>
                         <p><strong>ID:</strong> " . $row['id_juego'] . "</p>
                         <p><strong>Nombre:</strong> " . $row['nombre'] . "</p>
@@ -201,17 +209,16 @@ $menu = getMenu();
                         <p><strong>Ruta de Imagen:</strong> " . $row['ruta_imagen'] . "</p>
                         <button type='button' onclick='history.go(-1);' class='btn btn-secondary'>Volver</button>
                       </div>";
-            }
-            ?>
-            <footer>
-                <div class="container">
-                    <div class="col">
-                        <p class="lead text-center">&COPY Tienda Bowser, Todos los derechos reservados.</p>
-                    </div>
-                </div>
-            </footer>
+        }
+        ?>
+    </div>
+    <footer>
+        <div class="container">
+            <div class="col">
+                <p class="lead text-center">&COPY Tienda Bowser, Todos los derechos reservados.</p>
+            </div>
         </div>
-    </header>
+    </footer>
 </body>
 
 </html>
--- tienda_bowser/nosotros.php
+++ tienda_bowser/nosotros.php
@@ -7,31 +7,29 @@ $menu = getMenu();
 <html>
 
 <head th:fragment="head">
-    <meta charset="UTF-8" />
     <title>Tienda Bowser</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
     <script src="js/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-
 </head>
 
 <body class="body">
     <header th:fragment="header" class="header">
         <nav class="navbar navbar-expand-lg navbar-danger bg-dark">
             <a class="logo navbar-brand" href="#">
-                <img src="https://cdn.discordapp.com/attachments/1165381757655318588/1165405392671612969/3bW7WPi.png?ex=6546bb59&is=65344659&hm=ac23a5d55495c86e72cfb26cde8d26778547d6d94563612029d304492f5109c9&"
-                    class="logo" alt="Tienda Bowser">
+                <img src="https://cdn.discordapp.com/attachments/1165381757655318588/1165405392671612969/3bW7WPi.png?ex=6546bb59&is=65344659&hm=ac23a5d55495c86e72cfb26cde8d26778547d6d94563612029d304492f5109c9&" class="logo" alt="Tienda Bowser">
             </a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -44,8 +42,7 @@ $menu = getMenu();
                     <?php } ?>
                     <!-- Menú desplegable de "Consola" -->
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="consolaDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="consolaDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Consola
                         </a>
                         <div class="dropdown-menu bg-dark" aria-labelledby="consolaDropdown">
@@ -56,8 +53,7 @@ $menu = getMenu();
                     </li>
                     <!-- Menú desplegable de "Juego" -->
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="juegoDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="juegoDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Juego
                         </a>
                         <div class="dropdown-menu bg-dark" aria-labelledby="juegoDropdown">
@@ -68,8 +64,7 @@ $menu = getMenu();
                     </li>
                     <!-- Menú desplegable de "Accesorio" -->
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="accesorioDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="accesorioDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Accesorio
                         </a>
                         <div class="dropdown-menu bg-dark" aria-labelledby="accesorioDropdown">
@@ -84,8 +79,76 @@ $menu = getMenu();
                 <span id="themeIcon" class="fas fa-moon"></span>
             </button>
         </nav>
-
     </header>
+    <section>
+        <h3 class="titulonosotros">Nuestra Historia</h3>
+        <p class="historianosotros">Hace unos años, un grupo apasionado de entusiastas de los videojuegos decidió unir fuerzas para crear algo único: una tienda en línea especializada en productos inspirados en el famoso personaje de videojuegos, Bowser. Su amor por los juegos de la serie Super Mario Bros. y la fascinación por el icónico villano Bowser los llevaron a la idea de lanzar "Tienda Bowser".</p>
+        <p class="historianosotros">El equipo comenzó con una lluvia de ideas, definiendo qué tipo de productos querían ofrecer. Desde juegos de diferentes consolas, accesorios de consolas hasta consolas, la variedad de productos inspirados en Bowser era asombrosa. Decidieron que la tienda en línea no solo sería un lugar para comprar mercancía, sino también un espacio para que los fanáticos de videojuegos se conectaran y compartieran su entusiasmo.</p>
+    </section>
+    <style>
+        .accordion-button:not(.collapsed) {
+            background-color: black;
+            color: white;
+        }
+
+        .accordion-item.active strong {
+            background-color: black;
+            color: white;
+            /* Color del texto dentro de <strong> cuando el botón está abierto */
+        }
+    </style>
+    <h3 class="titulonosotros">Creadores</h3>
+    <div class="accordion" id="accordionPanelsStayOpenExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                    Adriana Viquez Jimenez
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                <div class="accordion-body">
+                    <strong>Mi nombre es Adriana Viquez</strong> Mi edad es de 25 años, vivo en Alajuela La Garita, Mis pasatiempos son jugar a videojuegos, ver series, y estudio Ingenieria en Sistemas
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                    Axel Noel Cabezas Cerda
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <strong>Mi nonbre es Axel Noel Cabezas Cerda</strong> Vivo en Heredia San Francisco, Tengo 19 años, Mis pasatiempos es jugar voleibol, ir al gym, tocar guitarra y estudio Ingeniería en Sistemas
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                    Enzo Leonardo Quesada Arand
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <strong>Mi nombre es Enzo Leonardo Quesada Arand</strong> Vivo en Sabana Norte, Tengo 20 años, Mis pasatiempos es ir al gym, ensamblar PCs y estudio Ingeniería en Sistemas
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
+                Ronald Alejandro Mendoza Wong
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                <strong>Mi nombre es Ronald Alejandro Mendoza Wong</strong> Vivo en San José Pavas, Tengo 22 años, Mis pasatiempos es ir al gym, escuchar música y los videojuegos y estudio Ingeniería en Sistemas
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
     <footer>
         <div class="container">
             <div class="col">

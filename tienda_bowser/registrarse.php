888bfa1077a09c6b7754358ac32188fc7afcd286
Arand2411 <enzoquesada2014@gmail.com>
2023-12-13 21:55:06 -0600

    ya sirve registrar
    ups

--- tienda_bowser/registrarse.php
+++ tienda_bowser/registrarse.php
@@ -12,6 +12,7 @@ if(!empty($_SESSION['usuario_autenticado'])) {
   
   }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include "RegistroUsuario/procesar_registro.php";
     include "Login/procesar_login.php";
 }
 ?>
